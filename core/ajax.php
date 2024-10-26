d9a6af1d20180b229f2835b3d5c413f0267cda10
Slava Abakumov <slava.abakumov@gmail.com>
2024-10-26 20:22:54 +0200

    improve code style and sanitizing in ajax.php

--- core/ajax.php
+++ core/ajax.php
@@ -8,6 +8,12 @@ function bpge_fields_set_delete() {
 
 	global $wpdb;
 
+	if ( ! current_user_can( 'manage_options' ) ) {
+		die;
+	}
+
+	check_admin_referer( 'bpge_manage_sets_nonce', 'nonce' );
+
 	switch_to_blog( bpge_get_main_site_id() );
 
 	$set_id = absint( $_POST['id'] ?? 0 );
@@ -30,7 +36,7 @@ function bpge_fields_set_delete() {
 add_action( 'wp_ajax_fields_set_delete', 'bpge_fields_set_delete' );
 
 /**
- * All main non-admin ajax requests.
+ * All front-end ajax requests.
  */
 function bpge_ajax() {
 
@@ -51,13 +57,15 @@ function bpge_ajax() {
 
 			// Reorder all fields according to new positions.
 			$i = 1;
+
 			foreach ( $field_order['position'] as $field_id ) {
+				// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery
 				$wpdb->update(
 					$wpdb->posts,
-					array( 'menu_order' => $i ),
-					array( 'ID' => (int) $field_id ),
-					array( '%d' ),
-					array( '%d' )
+					[ 'menu_order' => $i ],
+					[ 'ID' => (int) $field_id ],
+					[ '%d' ],
+					[ '%d' ]
 				);
 
 				clean_post_cache( (int) $field_id );
@@ -75,20 +83,21 @@ function bpge_ajax() {
 
 		case 'reorder_pages':
 			parse_str( $_REQUEST['page_order'], $page_order );
+
 			// Update menu_order for each gpage.
 			foreach ( $page_order['position'] as $index => $page_id ) {
 				wp_update_post(
-					array(
+					[
 						'ID'         => (int) $page_id,
 						'menu_order' => (int) $index,
-					)
+					]
 				);
 			}
 			$return = 'saved';
 			break;
 
 		case 'delete_page':
-			if ( $deleted = wp_delete_post( (int) $_REQUEST['page'], true ) ) {
+			if ( wp_delete_post( (int) $_REQUEST['page'], true ) ) {
 				$return = 'deleted';
 			} else {
 				$return = 'error';
@@ -97,23 +106,27 @@ function bpge_ajax() {
 
 		case 'apply_set':
 			$set_id = (int) $_REQUEST['set_id'];
+
 			if ( $set_id < 1 ) {
 				$return = 'error';
 				break;
 			}
 
 			// Get all fields for that set.
-			$fields    = new WP_Query(
-				array(
+			$fields = new WP_Query(
+				[
 					'post_parent' => $set_id,
 					'post_type'   => BPGE_FIELDS,
-				)
+				]
 			);
-			$to_import = array();
+
+			$to_import = [];
+
 			foreach ( $fields->posts as $field ) {
 				$field->desc    = $field->post_content;
 				$field->options = get_post_meta( $field->ID, 'bpge_field_options', true );
 				$field->display = get_post_meta( $field->ID, 'bpge_field_display', true );
+
 				unset(
 					$field->post_date,
 					$field->post_date_gmt,
@@ -126,25 +139,26 @@ function bpge_ajax() {
 					$field->filter,
 					$field->post_type
 				);
+
 				$to_import[] = $field;
 			}
 
 			// Get all groups ids.
 			$groups = groups_get_groups(
-				array(
+				[
 					'show_hidden'     => true,
 					'populate_extras' => false,
 					'per_page'        => 999,
-				)
+				]
 			);
 
-			// IUnsert in the loop all the fields where parent_id = group_id.
+			// Insert in the loop all the fields where parent_id = group_id.
 			foreach ( $groups['groups'] as $group ) {
 				foreach ( $to_import as $field ) {
 					$data                = (array) $field;
 					$data['post_parent'] = $group->id;
 					$data['post_type']   = BPGE_GFIELDS;
-					$data['post_status'] = $field->display == 'yes' ? 'publish' : 'draft';
+					$data['post_status'] = $field->display === 'yes' ? 'publish' : 'draft';
 					$field_id            = wp_insert_post( $data );
 
 					if ( is_int( $field_id ) ) {
@@ -153,17 +167,19 @@ function bpge_ajax() {
 
 						// Now save options.
 						if ( ! empty( $data['options'] ) ) {
-							$options = array();
+							$options = [];
+
 							foreach ( $data['options'] as $option ) {
-								$options[] = htmlspecialchars( strip_tags( $option ) );
+								$options[] = htmlspecialchars( wp_strip_all_tags( $option ) );
 							}
+
 							update_post_meta( $field_id, 'bpge_field_options', $options );
 						}
 					}
 				}
 			}
 
-			update_post_meta( $set_id, 'bpge_set_options', array( 'applied' => 'true' ) );
+			update_post_meta( $set_id, 'bpge_set_options', [ 'applied' => 'true' ] );
 
 			$return = 'success';
 			break;
@@ -171,14 +187,16 @@ function bpge_ajax() {
 		case 'dismiss_review':
 			$bpge             = bpge_get_options();
 			$bpge['reviewed'] = 'yes';
+
 			bp_update_option( 'bpge', $bpge );
+
 			$return = 'ok';
 			break;
 	}
 
 	restore_current_blog();
 
-	die( $return );
+	die( sanitize_key( $return ) );
 }
 
 add_action( 'wp_ajax_bpge', 'bpge_ajax' );
