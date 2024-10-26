f10f8ccbe7c1f70cdb9a305c2dda8f1fa9978a04
Slava Abakumov <slava.abakumov@gmail.com>
2024-10-26 22:54:04 +0200

    improve visual design, accessibility, code style in set_edit.php

--- views/admin/set_edit.php
+++ views/admin/set_edit.php
@@ -1,13 +1,25 @@
 <div id="box_edit_set_fields">
-	<h4><?php esc_html_e( 'Edit Set of Fields', 'buddypress-groups-extras' ); ?> &rarr; <span></span></h4>
+	<h4>
+		<?php esc_html_e( 'New Set of Fields', 'buddypress-groups-extras' ); ?>
+	</h4>
+
 	<div>
-		<label><?php esc_html_e( 'Name', 'buddypress-groups-extras' ); ?></label>
-		<input type="text" name="edit_set_fields_name" />
+		<label for="edit_set_fields_name">
+			<?php esc_html_e( 'Name', 'buddypress-groups-extras' ); ?>
+		</label>
+		<input type="text" id="edit_set_fields_name" name="edit_set_fields_name" />
 	</div>
+
 	<div>
-		<label><?php esc_html_e( 'Description', 'buddypress-groups-extras' ); ?></label>
-		<textarea name="edit_set_field_description"></textarea>
+		<label for="edit_set_field_description">
+			<?php esc_html_e( 'Description', 'buddypress-groups-extras' ); ?>
+		</label>
+		<textarea id="edit_set_field_description" name="edit_set_field_description"></textarea>
 	</div>
+
 	<input type="hidden" name="edit_set_fields_id" value="" />
-	<input id="editsf" type="submit" class="button-primary" name="editsetfields" value="<?php esc_attr_e( 'Edit Set of Fields', 'buddypress-groups-extras' ); ?>" />
+
+	<input id="editsf" type="submit" class="button-primary" name="editsetfields" value="<?php esc_attr_e( 'Save Changes', 'buddypress-groups-extras' ); ?>" />
+
+	<div class="clear"></div>
 </div>
