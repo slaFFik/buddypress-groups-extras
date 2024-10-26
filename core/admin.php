365862e3dd258904ef05898ffaf3c79dabe3256d
Slava Abakumov <slava.abakumov@gmail.com>
2024-10-26 20:23:22 +0200

    improve code style and escaping in admin.php

--- core/admin.php
+++ core/admin.php
@@ -15,7 +15,7 @@ class BPGE_ADMIN {
 	// Will be redefined after all tabs are loaded.
 	public $default_tab = null;
 	// The list of tabs in admin area, will be extended by child classes.
-	public $bpge_tabs = array();
+	public $bpge_tabs = [];
 	// Path the folder where all tabs are situated.
 	public $tabs_path = null;
 
@@ -29,10 +29,10 @@ class BPGE_ADMIN {
 		$this->tabs_path = __DIR__ . DS . 'admin_tabs';
 
 		// Own columns in gpages list.
-		add_filter( 'manage_' . BPGE_GPAGES . '_posts_columns', array( $this, 'manage_columns' ) );
-		add_action( 'manage_' . BPGE_GPAGES . '_posts_custom_column', array( $this, 'manage_columns_content' ), 10, 2 );
-		add_filter( 'page_row_actions', array( $this, 'manage_columns_actions' ), 10, 2 );
-		add_filter( 'bulk_actions-edit-' . BPGE_GPAGES, array( $this, 'manage_columns_remove_bulk' ) );
+		add_filter( 'manage_' . BPGE_GPAGES . '_posts_columns', [ $this, 'manage_columns' ] );
+		add_action( 'manage_' . BPGE_GPAGES . '_posts_custom_column', [ $this, 'manage_columns_content' ], 10, 2 );
+		add_filter( 'page_row_actions', [ $this, 'manage_columns_actions' ], 10, 2 );
+		add_filter( 'bulk_actions-edit-' . BPGE_GPAGES, [ $this, 'manage_columns_remove_bulk' ] );
 
 		// Create tabs.
 		$this->get_tabs();
@@ -45,36 +45,42 @@ class BPGE_ADMIN {
 	 */
 	public function manage_columns() {
 
-		return array(
+		return [
 			'cb'         => '<input type="checkbox" />',
 			'title'      => __( 'Title', 'buddypress-groups-extras' ),
 			'group_link' => __( 'Group Links', 'buddypress-groups-extras' ),
 			'page_link'  => __( 'Page Links', 'buddypress-groups-extras' ),
 			'date'       => __( 'Date', 'buddypress-groups-extras' ),
-		);
+		];
 	}
 
+	/**
+	 * Process column's content.
+	 *
+	 * @param string $column
+	 * @param int    $post_id
+	 */
 	public function manage_columns_content( $column, $post_id ) {
 
 		$group_id = get_post_meta( $post_id, 'group_id', true );
 		$post     = get_post( $post_id );
 
 		if ( empty( $group_id ) ) {
-			$groups_class  = new BP_Groups_Group;
+			$groups_class  = new BP_Groups_Group();
 			$groups_handle = $groups_class::get(
-				array(
+				[
 					'search_terms'    => $post->post_title,
-					'search_columns'  => array( 'name' ),
+					'search_columns'  => [ 'name' ],
 					'populate_extras' => false,
 					'show_hidden'     => true,
-				)
+				]
 			);
 
 			if ( ! empty( $groups_handle['groups'][0] ) ) {
 				$group = $groups_handle['groups'][0];
 			}
 		} else {
-			$group = groups_get_group( array( 'group_id' => $group_id ) );
+			$group = groups_get_group( $group_id );
 		}
 
 		if ( empty( $group ) ) {
@@ -85,29 +91,36 @@ class BPGE_ADMIN {
 		$group_admin_link = bp_get_group_admin_permalink( $group );
 
 		switch ( $column ) {
-			case 'group_link' :
+			case 'group_link':
 				if ( $post->post_parent !== 0 ) {
 					break;
 				}
 
-				echo '<a href="' . esc_url( $group_link ) . '" target="_blank">' . __( 'Visit', 'buddypress-groups-extras' ) . '</a>';
+				echo '<a href="' . esc_url( $group_link ) . '" target="_blank">' . esc_html__( 'Visit', 'buddypress-groups-extras' ) . '</a>';
 				echo ' | ';
-				echo '<a href="' . esc_url( $group_admin_link ) . '" target="_blank">' . __( 'Edit', 'buddypress-groups-extras' ) . '</a>';
-
+				echo '<a href="' . esc_url( $group_admin_link ) . '" target="_blank">' . esc_html__( 'Edit', 'buddypress-groups-extras' ) . '</a>';
 				break;
 
-			case 'page_link' :
+			case 'page_link':
 				if ( $post->post_parent === 0 ) {
 					break;
 				}
 
-				echo '<a href="' . esc_url( $group_link . BPGE_GPAGES . '/' . $post->post_name ) . '/" target="_blank">' . __( 'Visit', 'buddypress-groups-extras' ) . '</a>';
+				echo '<a href="' . esc_url( $group_link . BPGE_GPAGES . '/' . $post->post_name ) . '/" target="_blank">' . esc_html__( 'Visit', 'buddypress-groups-extras' ) . '</a>';
 				echo ' | ';
-				echo '<a href="' .esc_url( $group_admin_link . 'extras/pages-manage/?edit=' . $post_id ) . '" target="_blank">' . __( 'Edit', 'buddypress-groups-extras' ) . '</a>';
+				echo '<a href="' . esc_url( $group_admin_link . 'extras/pages-manage/?edit=' . $post_id ) . '" target="_blank">' . esc_html__( 'Edit', 'buddypress-groups-extras' ) . '</a>';
 				break;
 		}
 	}
 
+	/**
+	 * Process column bulk actions.
+	 *
+	 * @param array   $actions
+	 * @param WP_Post $post
+	 *
+	 * @return array
+	 */
 	public function manage_columns_actions( $actions, $post ) {
 
 		if ( $post->post_type !== BPGE_GPAGES ) {
@@ -126,7 +139,7 @@ class BPGE_ADMIN {
 	 */
 	public function manage_columns_remove_bulk() {
 
-		return array();
+		return [];
 	}
 
 	/**
@@ -135,13 +148,13 @@ class BPGE_ADMIN {
 	public function get_tabs() {
 
 		if ( $handle = opendir( $this->tabs_path ) ) {
-			while ( false !== ( $file = readdir( $handle ) ) ) {
-				if ( $file === "." || $file === ".." ) {
+			while ( ( $file = readdir( $handle ) ) !== false ) {
+				if ( $file === '.' || $file === '..' ) {
 					continue;
 				}
 
-				/** @noinspection PhpIncludeInspection */
-				$tab = include( $this->tabs_path . DS . $file );
+				$tab = include $this->tabs_path . DS . $file;
+
 				if ( $tab ) {
 					$this->bpge_tabs[] = $tab;
 				}
@@ -163,7 +176,7 @@ class BPGE_ADMIN {
 			return;
 		}
 
-		$tmp = array();
+		$tmp = [];
 
 		foreach ( $this->bpge_tabs as $tab ) {
 			$tmp[ $tab->position ] = $tab;
@@ -171,7 +184,9 @@ class BPGE_ADMIN {
 
 		// Make smaller position at the top of an array.
 		ksort( $tmp );
+
 		$this->bpge_tabs = $tmp;
+
 		unset( $tmp );
 
 		// Set the first tab as default.
@@ -188,19 +203,22 @@ class BPGE_ADMIN {
 
 		wp_enqueue_script( 'wp-pointer' );
 		wp_enqueue_style( 'wp-pointer' );
-		add_action( 'admin_footer', array( $this, 'load_pointers' ) );
+		add_action( 'admin_footer', [ $this, 'load_pointers' ] );
 
 		// All other admin area js.
-		add_action( 'admin_enqueue_scripts', array( $this, 'load_js' ) );
-		add_action( 'admin_enqueue_scripts', array( $this, 'load_css' ) );
+		add_action( 'admin_enqueue_scripts', [ $this, 'load_js' ] );
+		add_action( 'admin_enqueue_scripts', [ $this, 'load_css' ] );
 	}
 
+	/**
+	 * Custom pointers.
+	 */
 	public function load_pointers() {
 
 		$page = is_multisite() ? 'network/settings.php' : 'options-general.php';
 
-		$vote_content = '<h3>' . __( 'Vote for Features', 'buddypress-groups-extras' ) . '</h3>';
-		$vote_content .= '<p>' . __( 'Based on voting results I will implement features in new versions (either in core or as modules to extend the initial functionality).', 'buddypress-groups-extras' ) . '</p>';
+		$vote_content  = '<h3>' . __( 'Vote for Features', 'buddypress-groups-extras' ) . '</h3>';
+		$vote_content .= '<p>' . __( 'Based on voting results I may implement features in new versions (either in core or as modules to extend the initial functionality).', 'buddypress-groups-extras' ) . '</p>';
 
 		// Get all pointer that we dismissed.
 		$dismissed = explode( ',', (string) get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true ) );
@@ -233,18 +251,28 @@ class BPGE_ADMIN {
 		}
 	}
 
+	/**
+	 * Admin-area-related JS code.
+	 *
+	 * @param string $hook
+	 */
 	public function load_js( $hook ) {
 
 		if ( $hook !== 'settings_page_bpge-admin' ) {
 			return;
 		}
 
-		wp_enqueue_script( 'bpge_admin_js_popup', BPGE_URL . '/libs/messi.js', array( 'jquery' ), BPGE_VERSION );
+		wp_enqueue_script( 'bpge_admin_js_popup', BPGE_URL . '/libs/messi.js', [ 'jquery' ], BPGE_VERSION, false );
 
-		wp_enqueue_script( 'bpge-admin', BPGE_URL . '/js/admin-scripts.js', array( 'wp-pointer' ), BPGE_VERSION );
+		wp_enqueue_script( 'bpge-admin', BPGE_URL . '/js/admin-scripts.js', [ 'wp-pointer' ], BPGE_VERSION, false );
 		wp_localize_script( 'bpge-admin', 'bpge', bpge_get_localized_data() );
 	}
 
+	/**
+	 * Admin-area-related CSS code.
+	 *
+	 * @param string $hook
+	 */
 	public function load_css( $hook ) {
 
 		global $post_type;
@@ -254,21 +282,22 @@ class BPGE_ADMIN {
 			( isset( $post_type ) && $post_type === 'gpages' ) ||
 			$hook === 'settings_page_bpge-admin'
 		) {
-			wp_enqueue_style( 'bpge_admin_css', BPGE_URL . '/css/admin-styles.css' );
+			wp_enqueue_style( 'bpge_admin_css', BPGE_URL . '/css/admin-styles.css', [], BPGE_VERSION );
 		}
 
 		if ( $hook === 'settings_page_bpge-admin' ) {
-			wp_enqueue_style( 'bpge_admin_css_messi', BPGE_URL . '/libs/messi.css' );
+			wp_enqueue_style( 'bpge_admin_css_messi', BPGE_URL . '/libs/messi.css', [], BPGE_VERSION );
 		}
 	}
 
 	/**
-	 * Actual html of a page (its core).
+	 * Actual HTML of a page (its core).
 	 */
 	public function admin_page() {
 
 		// Define some data that can be given to each metabox during rendering.
-		$tab = $this->get_cur_tab(); ?>
+		$tab = $this->get_cur_tab();
+        ?>
 
 		<div id="bpge-admin" class="wrap">
 			<?php $this->header(); ?>
@@ -276,9 +305,12 @@ class BPGE_ADMIN {
 			<?php
 			$page     = is_multisite() ? 'network/settings.php' : 'options-general.php';
 			$page_url = admin_url( '/' . $page . '?page=' . BPGE_ADMIN_SLUG );
+
+			// phpcs:disable WordPress.Security.NonceVerification.Recommended
 			if ( isset( $_GET['tab'] ) && ! empty( $_GET['tab'] ) ) {
 				$page_url .= '&tab=' . sanitize_key( $_GET['tab'] );
 			}
+			// phpcs:enable WordPress.Security.NonceVerification.Recommended
 			?>
 
 			<form action="<?php echo esc_url( $page_url ); ?>" class="tab_<?php echo esc_attr( $tab ); ?>" id="bpge-form" method="post">
@@ -299,9 +331,11 @@ class BPGE_ADMIN {
 	 */
 	public function get_cur_tab() {
 
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended
 		if ( isset( $_GET['tab'] ) && ! empty( $_GET['tab'] ) ) {
-			return $_GET['tab'];
+			return sanitize_key( $_GET['tab'] );
 		}
+		// phpcs:enable WordPress.Security.NonceVerification.Recommended
 
 		return $this->default_tab;
 	}
@@ -314,34 +348,40 @@ class BPGE_ADMIN {
 		$current_tab = $this->get_cur_tab();
 
 		echo '<h2>';
-		_e( 'BuddyPress Groups Extras', 'buddypress-groups-extras' );
-		do_action( 'bpge_admin_header_title_pro' );
-		echo '&rarr; ';
-		_e( 'Extend Your Groups', 'buddypress-groups-extras' );
-		do_action( 'bpge_admin_header_title' );
-		echo '</h2>'; ?>
+			esc_html_e( 'BuddyPress Groups Extras', 'buddypress-groups-extras' );
+			do_action( 'bpge_admin_header_title_pro' );
+			echo '&rarr; ';
+			esc_html_e( 'Extend Your Groups', 'buddypress-groups-extras' );
+			do_action( 'bpge_admin_header_title' );
+		echo '</h2>';
+        ?>
 
 		<?php
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		if ( isset( $_GET['saved'] ) ) {
 			echo '<div style="clear:both"></div>';
-			echo '<div id="message" class="updated fade"><p>' . __( 'All changes were saved. Go and check results!', 'buddypress-groups-extras' ) . '</p></div>';
+			echo '<div id="message" class="updated fade"><p>' . esc_html__( 'All changes were saved. Go and check results!', 'buddypress-groups-extras' ) . '</p></div>';
 		}
 
 		if ( ! isset( $this->bpge['reviewed'] ) || $this->bpge['reviewed'] === 'no' ) {
 			echo '<div style="clear:both"></div>';
 			echo '<div id="message" class="notice notice-info fade"><p>' .
-			     sprintf( /* translators: %s - URL to WordPress.org page where use can review the plugin. */
-				     __( 'If you like the plugin please review it on its <a href="%s">WordPress Repository page</a>. Thanks in advance!', 'buddypress-groups-extras' ),
-				     'https://wordpress.org/support/view/plugin-reviews/buddypress-groups-extras'
-			     ) .
-			     '<span style="float:right"><a href="#" class="bpge_review_dismiss" style="color:red">' . __( 'Dismiss', 'buddypress-groups-extras' ) . '</span>' .
-			     '</p></div>';
+				sprintf(
+					wp_kses( /* translators: %s - URL to WordPress.org page where use can review the plugin. */
+						__( 'If you like the plugin please review it on its <a href="%s" target="_blank">WordPress Repository page</a>. Thanks in advance!', 'buddypress-groups-extras' ),
+						[ 'a' => [ 'href' => [] ] ]
+					),
+					'https://wordpress.org/support/view/plugin-reviews/buddypress-groups-extras'
+				) .
+				'<span style="float:right"><a href="#" class="bpge_review_dismiss" style="color:red">' . esc_html__( 'Dismiss', 'buddypress-groups-extras' ) . '</span>' .
+				'</p></div>';
 		}
 
 		echo '<div style="clear:both"></div>';
 		echo '<h3 class="nav-tab-wrapper">';
 		foreach ( $this->bpge_tabs as $tab ) {
 			$active = $current_tab === $tab->slug ? 'nav-tab-active' : '';
+
 			echo '<a class="nav-tab ' . esc_attr( $active ) . '" id="bpge_tab_' . esc_attr( $tab->slug ) . '" href="?page=' . esc_attr( $this->slug ) . '&tab=' . esc_attr( $tab->slug ) . '">' . esc_html( $tab->title ) . '</a>';
 		}
 		do_action( 'bpge_admin_tabs_links' );
@@ -365,13 +405,14 @@ class BPGE_ADMIN_TAB {
 	public $slug     = null;
 
 	// Used by some pro extensions.
-	public $extras_to_header = array();
+	public $extras_to_header = [];
 
 	/**
 	 * Create the actual page object.
 	 */
 	public function __construct() {
 
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		if ( ! ( isset( $_GET['page'] ) && $_GET['page'] === BPGE_ADMIN_SLUG ) ) {
 			return;
 		}
@@ -381,26 +422,31 @@ class BPGE_ADMIN_TAB {
 
 		register_setting( $this->slug, $this->slug );
 		add_settings_section(
-			$this->slug . '_settings', // section id
-			'', // title
-			array( &$this, 'display' ), // method handler
-			$this->slug // slug
+			$this->slug . '_settings',
+			'',
+			[ &$this, 'display' ],
+			$this->slug
 		);
 
 		$this->register_sections();
 
-		if ( $test = $this->header_title_attach() ) {
+		$test = $this->header_title_attach();
+
+		if ( $test ) {
 			$this->extras_to_header[] = $test;
 		}
 
-		add_action( 'bpge_admin_header_title', array( $this, 'apply_header_extras' ), 10 );
+		add_action( 'bpge_admin_header_title', [ $this, 'apply_header_extras' ], 10 );
 
 		$tab = 'general';
+
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
 		if ( isset( $_GET['tab'] ) ) {
-			$tab = $_GET['tab'];
+			$tab = sanitize_key( $_GET['tab'] );
 		}
 
 		// Process save process.
+		// phpcs:disable WordPress.Security.NonceVerification.Recommended,WordPress.Security.NonceVerification.Missing
 		if (
 			! empty( $_POST )
 			&& is_admin()
@@ -409,8 +455,9 @@ class BPGE_ADMIN_TAB {
 		) {
 			$this->save();
 			// Now redirect to the same page to clear POST.
-			wp_redirect( str_replace( '&saved', '', $_POST['_wp_http_referer'] ) . '&saved' );
+			wp_safe_redirect( str_replace( '&saved', '', wp_unslash( $_POST['_wp_http_referer'] ) ) . '&saved' );
 		}
+		// phpcs:enable WordPress.Security.NonceVerification.Recommended
 	}
 
 	/**
@@ -432,10 +479,15 @@ class BPGE_ADMIN_TAB {
 		return '';
 	}
 
+	/**
+	 * Whether header extra strings should be applied.
+	 */
 	public function apply_header_extras() {
 
-		if ( $data = array_filter( $this->extras_to_header ) ) {
-			echo ' [' . implode( ', ', $data ) . ']';
+		$data = array_filter( $this->extras_to_header );
+
+		if ( $data ) {
+			echo ' [' . esc_html( implode( ', ', $data ) ) . ']';
 		}
 	}
 
